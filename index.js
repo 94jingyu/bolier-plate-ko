4c7f2dc9d69ea6c574bf77eb7868117229ca0a81
jingyu <94jingyu@gmail.com>
2020-08-29 01:58:01 +0900

    #7 BodyParser & PostMan & 회원 가입 기능

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+// bodyParser는 Client로부터 오는 정보를 분석할 수 있다.
+// application/x-www-form-urlencoded 타입의 정보를 분석 가능
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json 타입의 정보를 분석 가능
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 const MONGODB_URL = 'mongodb+srv://jingyu:1234@boilerplate.es7fi.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+.catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {  res.send('Hello World!') })
+
+
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 Client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
+
+
+
 
 app.listen(port, () => {  console.log(`Example app listening at http://localhost:${port}`) })
 
